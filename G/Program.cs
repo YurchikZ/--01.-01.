5f43705e449a609a340a2bc1a0f02e8f70ed121b
YurchikZ <nikei15212@gmail.com>
2024-09-17 13:33:06 +0300

    Я решил первую задачу

--- G/Program.cs
+++ G/Program.cs
@@ -10,7 +10,31 @@ namespace G
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("А я коко джамбо ай яй я");
+            // Вариант 3
+            List<int>ints = new List<int>();
+            int x = 0;
+            for (int i = -14; i < 25; i+= 6) // Цикл для заполнения списка + вывод списка
+            {
+                ints.Add(i);
+                Console.Write(ints[x] + " ");
+                x++;  
+            }
+            Console.WriteLine();
+            for (int i = 0; i < ints.Count; i++)  // Цикл для удаления значений в диапозоне [-5 10)
+            {
+                if (ints[i] >= -5 && ints[i] < 10) 
+                {
+                    Console.Write(ints[i] + " ");
+                    ints.RemoveAt(i);
+                    i--;
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < ints.Count; i++) // вывод списка
+            {
+                Console.Write(ints[i] + " ");
+            }
+
             Console.ReadKey();
         }
     }
